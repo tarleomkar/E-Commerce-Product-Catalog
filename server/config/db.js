92c56e5b882e0cd00ebfe458ab5ff797d19109ca
mayurianu23 <tarlemayuri74@gmail.com>
2025-01-01 17:33:27 +0530

    ss

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://user1:Omkar2546@cluster0.iapi7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0NODE_ENV=development');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);

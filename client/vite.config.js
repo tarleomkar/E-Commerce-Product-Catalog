d3566aa8e0ebb77cd52882d0c824511f6ed5a22b
mayurianu23 <tarlemayuri74@gmail.com>
2025-01-02 00:01:49 +0530

    deploying frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures relative paths for assets
   build: {
     rollupOptions: {
       external: ['@reduxjs/toolkit'], // Ensure proper externalization
     },
   },
 });
+
+
